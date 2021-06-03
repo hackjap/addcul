8ff4c2ebcf730386dbd3ada8c957eb8b38ff7e06
ChoiJunBae <cjb9610@naver.com>
2021-06-03 17:47:01 +0900

    [Updated] K문화 shop 모든 작업 완료

--- app/src/main/java/com/example/addcul/Util/DBHelper.java
+++ app/src/main/java/com/example/addcul/Util/DBHelper.java
@@ -5,7 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
-    public static final int VERSION = 83;
+    public static final int VERSION = 86;
 
     public DBHelper(Context context) {
         super(context, "addculDB", null, VERSION);
@@ -33,6 +33,9 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL("insert into newWord(num, name, info, subInfo)values ('9','갑통알', '[ 갑자기 통장을 보니 알바해야겠다 ]', '-')");
         db.execSQL("insert into newWord(num, name, info, subInfo)values ('10','애빼시', '[ 애교 빼면 시체 ]', '-')");
 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
         //쇼핑 테이블
         String shopSQL = "create table shopWomen(" +
                 "_id integer primary key autoincrement," +
@@ -53,11 +56,66 @@ public class DBHelper extends SQLiteOpenHelper {
                 "tag text," +
                 "photo text);";
         db.execSQL(shopSQL2);
-        db.execSQL("insert into shopMen(name, tag, photo)values ('무신사','#1+1  #베이직 의류', 'img_shop_midasb')");
-        db.execSQL("insert into shopMen(name, tag, photo)values ('바이더알','#고급적 #중저가  #오피스룩', 'img_shop_loveparis')");
-        db.execSQL("insert into shopMen(name, tag, photo)values ('멋남','#청바지  #배기진', 'img_shop_justone')");
-        db.execSQL("insert into shopMen(name, tag, photo)values ('슈퍼스타아이','#독특한 스타일  #호불호', 'img_shop_mocobling')");
-        db.execSQL("insert into shopMen(name, tag, photo)values ('지니푸','#데일리룩 #키에 따른 바지 사이즈', 'img_shop_pippin')");
+        db.execSQL("insert into shopMen(name, tag, photo)values ('무신사','#종합 쇼핑몰  #무료 배송', 'img_shop_musinsa')");
+        db.execSQL("insert into shopMen(name, tag, photo)values ('조군샵','#데일리룩 추천  #셔츠 맛집', 'img_shop_jogunshop')");
+        db.execSQL("insert into shopMen(name, tag, photo)values ('바이더알','#자체제작  #셀렉트샵', 'img_shop_byther')");
+        db.execSQL("insert into shopMen(name, tag, photo)values ('모던이프','#아우터  #1+1', 'img_shop_modernif')");
+        db.execSQL("insert into shopMen(name, tag, photo)values ('슈퍼스타아이','#1+1+1  #코디 세트', 'img_shop_superstari')");
+        db.execSQL("insert into shopMen(name, tag, photo)values ('락커룸','#신상 맛집  #저렴한 가격', 'img_shop_locker')");
+
+        String shopSQL3 = "create table shopApp(" +
+                "_id integer primary key autoincrement," +
+                "name text," +
+                "tag text," +
+                "photo text);";
+        db.execSQL(shopSQL3);
+        db.execSQL("insert into shopApp(name, tag, photo)values ('다나와','#가격, 성능비교  #최저가판매', 'img_shop_danawa')");
+        db.execSQL("insert into shopApp(name, tag, photo)values ('삼성전자','#믿고 쓰는 삼성  #케어플러스', 'img_shop_samsung')");
+        db.execSQL("insert into shopApp(name, tag, photo)values ('LG전자','#케어솔루션  #LG 가전제품은 최고', 'img_shop_lg')");
+        db.execSQL("insert into shopApp(name, tag, photo)values ('하이마트','#2시간 퀵배송  #종합 쇼핑몰', 'img_shop_himart')");
+        db.execSQL("insert into shopApp(name, tag, photo)values ('전자랜드','#매일 새로운 이벤트  #생활일착 컨텐츠', 'img_shop_junjaland')");
+
+        String shopSQL4 = "create table shopMake(" +
+                "_id integer primary key autoincrement," +
+                "name text," +
+                "tag text," +
+                "photo text);";
+        db.execSQL(shopSQL4);
+        db.execSQL("insert into shopMake(name, tag, photo)values ('미샤','#전품목 1+1  #조기품절 주의', 'img_shop_misha')");
+        db.execSQL("insert into shopMake(name, tag, photo)values ('토니모리','#푸짐한 사은품  #수분크림으로 유명', 'img_shop_tonymori')");
+        db.execSQL("insert into shopMake(name, tag, photo)values ('데페이스샵','#선크림 유명  #선크림 전품목 1+1 ', 'img_shop_thefaceshop')");
+        db.execSQL("insert into shopMake(name, tag, photo)values ('이니스프리','#필 마스크 맛집  #여름쿠션 맛집', 'img_shop_innisfree')");
+        db.execSQL("insert into shopMake(name, tag, photo)values ('에뛰드','#매일 신상품 출시  #셀렉샵', 'img_shop_etude')");
+
+        String shopSQL5 = "create table shopFood(" +
+                "_id integer primary key autoincrement," +
+                "name text," +
+                "tag text," +
+                "photo text);";
+        db.execSQL(shopSQL5);
+        db.execSQL("insert into shopFood(name, tag, photo)values ('노브랜드','#초저가  #가격대비 최상 품질', 'img_shop_nobrand')");
+        db.execSQL("insert into shopFood(name, tag, photo)values ('홈플러스24','#품목별 깔끔한 안내  #빠른 배송', 'img_shop_homeplus')");
+        db.execSQL("insert into shopFood(name, tag, photo)values ('쿠팡','#로켓 배송  #떠오르는 기업', 'img_shop_coupang')");
+        db.execSQL("insert into shopFood(name, tag, photo)values ('마켓컬리','#첫 구매시 무료배송  #신규회원 인기상품 100원', 'img_shop_marketkurly')");
+        db.execSQL("insert into shopFood(name, tag, photo)values ('푸짐','#깔끔한 페이지 디자인  #도심 속 직배송', 'img_shop_foodzim')");
+
+        String shopSQL6 = "create table shopFurni(" +
+                "_id integer primary key autoincrement," +
+                "name text," +
+                "tag text," +
+                "photo text);";
+        db.execSQL(shopSQL6);
+        db.execSQL("insert into shopFurni(name, tag, photo)values ('이케아','#더 낮은 새로운 가격  #가구 쇼핑몰의 대명사', 'img_shop_ikea')");
+        db.execSQL("insert into shopFurni(name, tag, photo)values ('오늘의집','#베스트 상품 최대 80% 할인  #방꾸미기', 'img_shop_todayhouse')");
+        db.execSQL("insert into shopFurni(name, tag, photo)values ('다이소','#싼 가격  #좋은 품질', 'img_shop_daiso')");
+        db.execSQL("insert into shopFurni(name, tag, photo)values ('모던하우스','#신규가입 10%할인  #셀럽이 제안하는 핫 가구', 'img_shop_modernhouse')");
+        db.execSQL("insert into shopFurni(name, tag, photo)values ('까사미아','#모던한 디자인  #하이퀄리티', 'img_shop_casamia')");
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 
         //일상 테이블
         String lifeSQL = "create table life(" +
@@ -102,6 +160,10 @@ public class DBHelper extends SQLiteOpenHelper {
             db.execSQL("drop table newWord");
             db.execSQL("drop table shopWomen");
             db.execSQL("drop table shopMen");
+            db.execSQL("drop table shopApp");
+            db.execSQL("drop table shopMake");
+            db.execSQL("drop table shopFood");
+            db.execSQL("drop table shopFurni");
             db.execSQL("drop table life");
             db.execSQL("drop table problem");
             db.execSQL("drop table travel");
--- app/src/main/java/com/example/addcul/fragment/FragShop.java
+++ app/src/main/java/com/example/addcul/fragment/FragShop.java
@@ -139,7 +139,6 @@ public class FragShop extends Fragment {
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-
             switch (v.getId()) {
                 case R.id.frag_shop_women_btn: {
                     setBtnBackColor(btnWomen);
@@ -185,32 +184,31 @@ public class FragShop extends Fragment {
                         public void onTouch(View v, int position) {
 
                             if (position == 0) {
-                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.croquis.zigzag&referrer=af_tranid%3DaALbSNrhALd2UfOvR5ctCw%26shortlink%3D189189a0%26pid%3Dzigzag_website_aos%26af_click_lookback%3D1d%26af_web_id%3D98a651ce-1f5c-41e4-950c-d741ac07bc7b-o%26utm_source%3Dzigzag_website_aos"));
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://display.musinsa.com/display/brands/mahagrid/goods?gclid=Cj0KCQjw2NyFBhDoARIsAMtHtZ4owa1L1VRtdeN-Y_j-2Fp6AiWDFVLB5KcR-86RG3DjqO59ufieA5caAmebEALw_wcB"));
+                                startActivity(intent);
+                            } else if (position == 1) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.jogunshop.com/"));
+                                startActivity(intent);
+                            }else if (position == 2) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://byther.kr/"));
+                                startActivity(intent);
+                            }else if (position == 3) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://modernif.co.kr/"));
+                                startActivity(intent);
+                            }else if (position == 4) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.superstari.co.kr/?gclid=Cj0KCQjw2NyFBhDoARIsAMtHtZ4EP10SYD7u84b8NOY_6cyCRppUySVPRdLILScA05qOkcqlblvPg54aAoY4EALw_wcB"));
+                                startActivity(intent);
+                            } else if (position == 5) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://locker-room.co.kr/"));
                                 startActivity(intent);
                             }
-//                            } else if (position == 1) {
-//                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.midasb.co.kr/"));
-//                                startActivity(intent);
-//                            }else if (position == 2) {
-//                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://loveparis.net/"));
-//                                startActivity(intent);
-//                            }else if (position == 3) {
-//                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://justone.co.kr/?enlipleMBDCEnc=bm89MCZrbm89MCZrd3JkU2VxPTI0MzQxOTU1JnM9MjIzNzAmYWRndWJ1bj1HRyZnYj1HRyZzYz01ZGM4ZTA4NDE4NDk0OWRkOTlhOTE4NWViMDJiN2I5YiZtYz0yMjM3MCZ1PWp1c3RvbmUmcHJvZHVjdD1uY3Q="));
-//                                startActivity(intent);
-//                            }else if (position == 4) {
-//                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.mocobling.com/"));
-//                                startActivity(intent);
-//                            } else if (position == 5) {
-//                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.pippin.co.kr/?da_ref=Yz10UnNFVVI=&enlipleMBDCEnc=bm89MCZrbm89MCZrd3JkU2VxPTI1NTk0NzI4JnM9MjIzNzAmYWRndWJ1bj1HRyZnYj1HRyZzYz01MzVhNDg2MjFmMWM0MDU5OWQ4N2Y4M2RiYTBlNDQxOCZtYz0yMjM3MCZ1PXBpcHBpbjExJnByb2R1Y3Q9bmN0&gclid=Cj0KCQjw2NyFBhDoARIsAMtHtZ4FYF5gtg7oBGGZ6TCGklsQ37amY0XsHQxAU13i8hK0xkIGZcGf-I8aAicYEALw_wcB"));
-//                                startActivity(intent);
-//                            }
                         }
                     });
                     break;
                 }
                 case R.id.frag_shop_appliance_btn:{
                     setBtnBackColor(btnAppliance);
-                    String tableName = "shopMen";
+                    String tableName = "shopApp";
                     getDatabase(tableName);
                     shopAdapter = new ShopAdapter(list, getActivity());
                     recyclerView.setAdapter(shopAdapter);
@@ -220,10 +218,19 @@ public class FragShop extends Fragment {
                         public void onTouch(View v, int position) {
 
                             if (position == 0) {
-                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.midasb.co.kr/"));
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.danawa.com/?src=adwords&kw=GA0000020&gclid=Cj0KCQjw2NyFBhDoARIsAMtHtZ4a0q2xeoXczP9z2FeNGdSwmXnCyp6OrzVdOuHJzVdsyRd3zEAizMAaArksEALw_wcB"));
                                 startActivity(intent);
                             } else if (position == 1) {
-                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.naver.com/"));
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.samsung.com/sec/"));
+                                startActivity(intent);
+                            }else if (position == 2) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.lge.co.kr/"));
+                                startActivity(intent);
+                            }else if (position == 3) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.e-himart.co.kr/app/display/showDisplayShop?originReferrer=himartindex"));
+                                startActivity(intent);
+                            }else if (position == 4) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.etlandmall.co.kr/mobile/main/index.do"));
                                 startActivity(intent);
                             }
 
@@ -233,7 +240,7 @@ public class FragShop extends Fragment {
                 }
                 case R.id.frag_shop_makeUp_btn:{
                     setBtnBackColor(btnMakeUp);
-                    String tableName = "shopMen";
+                    String tableName = "shopMake";
                     getDatabase(tableName);
                     shopAdapter = new ShopAdapter(list, getActivity());
                     recyclerView.setAdapter(shopAdapter);
@@ -243,10 +250,19 @@ public class FragShop extends Fragment {
                         public void onTouch(View v, int position) {
 
                             if (position == 0) {
-                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.midasb.co.kr/"));
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.mynunc.com/brand/missha?utm_source=google&utm_medium=sa&utm_campaign=N%5FBRAND&utm_term=%EB%AF%B8%EC%83%A4&_C_=611&_AT=036B126E018002FE8732&gclid=Cj0KCQjw--GFBhDeARIsACH_kdaxWwY-_8YA6xJkujjlhlR0__61VHWYPtRrGKEXqFl2O5ijOgWyK5EaAiMwEALw_wcB"));
                                 startActivity(intent);
                             } else if (position == 1) {
-                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.naver.com/"));
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.tonystreet.com/"));
+                                startActivity(intent);
+                            }else if (position == 2) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.naturecollection.com/mall/brand.jsp?cate_seq=662"));
+                                startActivity(intent);
+                            }else if (position == 3) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.innisfree.com/kr/ko/Main.do?utm_campaign=madit_conversion_al&utm_medium=sa&utm_source=google&utm_content=na&utm_trg=brand&utm_term=%EC%9D%B4%EB%8B%88%EC%8A%A4%ED%94%84%EB%A6%AC&gclid=Cj0KCQjw--GFBhDeARIsACH_kdY3im6DO5yx-IocL58vijz82crmPlErsP196fvhwcLuxGJNdOg0tT8aAnl7EALw_wcB"));
+                                startActivity(intent);
+                            }else if (position == 4) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.etude.com/kr/ko/main"));
                                 startActivity(intent);
                             }
 
@@ -256,7 +272,7 @@ public class FragShop extends Fragment {
                 }
                 case R.id.frag_shop_food_btn:{
                     setBtnBackColor(btnFood);
-                    String tableName = "shopMen";
+                    String tableName = "shopFood";
                     getDatabase(tableName);
                     shopAdapter = new ShopAdapter(list, getActivity());
                     recyclerView.setAdapter(shopAdapter);
@@ -266,10 +282,19 @@ public class FragShop extends Fragment {
                         public void onTouch(View v, int position) {
 
                             if (position == 0) {
-                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.midasb.co.kr/"));
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://emart.ssg.com/specialStore/nobrand/main.ssg?ckwhere=e_ggbr&EKAMS=google.272.5085.20504.2034359.602460933&trackingDays=1&gclid=Cj0KCQjw--GFBhDeARIsACH_kdaawBbcqDaHs4Ya851IEbt6vSdegfm1zGMbkl5ag3g-q-P5mY9P4K0aAksxEALw_wcB"));
                                 startActivity(intent);
                             } else if (position == 1) {
-                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.naver.com/"));
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://front.homeplus.co.kr/leaflet?gnbNo=92"));
+                                startActivity(intent);
+                            }else if (position == 2) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.coupang.com/np/categories/194276"));
+                                startActivity(intent);
+                            }else if (position == 3) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kurly.com/shop/main/index.php?&utm_source=1055&utm_medium=2106&utm_campaign=joinpage&utm_term=%EC%BB%AC%EB%A6%AC&utm_content=brand&gclid=Cj0KCQjw--GFBhDeARIsACH_kdaA7PL4k1um_cf_qq5i9yNmtjm7ly3LKyDXQbrggZRmNuvq49LuHsYaAsUxEALw_wcB"));
+                                startActivity(intent);
+                            }else if (position == 4) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://foodzim.co.kr/?gclid=Cj0KCQjw--GFBhDeARIsACH_kdYaT1YnDxuMF0FT0AjA8vUKYfgKtTh97QOdOWr_BSj6ACWE4WFyWBQaAligEALw_wcB"));
                                 startActivity(intent);
                             }
 
@@ -279,7 +304,7 @@ public class FragShop extends Fragment {
                 }
                 case R.id.frag_shop_furniture_btn:{
                     setBtnBackColor(btnFurniture);
-                    String tableName = "shopMen";
+                    String tableName = "shopFurni";
                     getDatabase(tableName);
                     shopAdapter = new ShopAdapter(list, getActivity());
                     recyclerView.setAdapter(shopAdapter);
@@ -289,10 +314,19 @@ public class FragShop extends Fragment {
                         public void onTouch(View v, int position) {
 
                             if (position == 0) {
-                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.midasb.co.kr/"));
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ikea.com/kr/ko/?utm_source=google&utm_medium=cpc&utm_campaign=sa_go_pc&utm_content=1-1.%EB%B8%8C%EB%9E%9C%EB%93%9C_%EB%B8%8C%EB%9E%9C%EB%93%9C&utm_term=%EC%9D%B4%EC%BC%80%EC%95%84&gclid=Cj0KCQjw--GFBhDeARIsACH_kdZZlqNMfOjwtMvvpc6Jjq5cBVVzYUfxv3bNo2nUKEX19rX_QcM-ZFYaAvDsEALw_wcB"));
                                 startActivity(intent);
                             } else if (position == 1) {
-                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.naver.com/"));
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://ohou.se/store?utm_source=brand_google&utm_medium=cpc&utm_campaign=commerce&utm_content=e&utm_term=%EC%98%A4%EB%8A%98%EC%9D%98%EC%A7%91&source=14&affect_type=UtmUrl&gclid=Cj0KCQjw--GFBhDeARIsACH_kdbSn1AMl7bwwUlssYlgN9BvgVGQkJAnvbiZgw_FV6jB6bDdeA8keDUaAsHjEALw_wcB"));
+                                startActivity(intent);
+                            }else if (position == 2) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.daisomall.co.kr/"));
+                                startActivity(intent);
+                            }else if (position == 3) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.modernhousemall.com/main/index.php?utm_source=google&utm_medium=SA_PC&utm_campaign=modernhouse&utm_term=%EB%AA%A8%EB%8D%98%ED%95%98%EC%9A%B0%EC%8A%A4"));
+                                startActivity(intent);
+                            }else if (position == 4) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.guud.com/index?utm_source=google&utm_campaign=searchad&utm_medium=cpc&utm_term=%EA%B9%8C%EC%82%AC%EB%AF%B8%EC%95%84&epe_vid=103&epe_tcd=156645561&gclid=Cj0KCQjw--GFBhDeARIsACH_kdbYnWnGuH4e8VchxCeDqksunwpHv3-AZK1pJoTVbnsE7egRRn2zwfIaAvokEALw_wcB"));
                                 startActivity(intent);
                             }
 
@@ -300,9 +334,6 @@ public class FragShop extends Fragment {
                     });
                     break;
                 }
-
-
-
             }
         }
     };
