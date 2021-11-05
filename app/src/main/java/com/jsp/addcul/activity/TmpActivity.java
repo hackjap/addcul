d2dfc66be9676a41ca39749974b9b94014f90b7d
hackjap <hackjap1015@gmail.com>
2021-11-05 18:01:21 +0900

    :오랜만에 업데이트 ㅎㅎ

--- app/src/main/java/com/jsp/addcul/activity/TmpActivity.java
+++ app/src/main/java/com/jsp/addcul/activity/TmpActivity.java
@@ -1,5 +1,6 @@
 package com.jsp.addcul.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -24,50 +25,64 @@ public class TmpActivity extends AppCompatActivity {
     3.어뎁터
     */
 
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tmp);
 
-        RecyclerView recyclerView = findViewById(R.id.tmp_recyclerview);
-
-        // 리사이클러에 담을 문자열 배열 생성
-        String names[] = {"Jang", "sung", "Lee", "kim"};
-        ArrayList<String> list = new ArrayList<>();
-        for (String name:names) {
-            list.add(name);
-        }
-        Log.e("리스트 ", list.toString());
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new MyAdapter(list));
-
+        setRecyclerAndList(); // 리사이클러 셋팅
 
     }
 
-    // 리사이클러뷰에 연결할 어뎁터
+    private ArrayList<String> getNameList() {
+        String names[] = {"장성필", "윤병준", "최준배"};
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (String name : names) {
+            list.add(name);
+        }
+        return list;
+    }
+
+    private void setRecyclerAndList(){
+        ArrayList<String> list = getNameList();
+
+        recyclerView = findViewById(R.id.temp_recycler1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new MyAdapter(list));
+    }
+
+
     private class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
 
-        public ArrayList<String>list;
+        // 데이터와 리스트 연결
+        ArrayList<String> list;
+
         public MyAdapter(ArrayList<String> list) {
             this.list = list;
         }
 
-
-        // ViewHolder를 연결함
-        @Override
+        @NonNull
+        @Override // 레이아웃 연결
         public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_tmp, parent, false);
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_item, parent, false);
             return new MyViewHolder(view);
-
         }
 
         @Override
         public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+
             String name = list.get(position);
-            holder.name.setText(name);
+
+            holder.text1.setText(name + 1);
+            holder.text2.setText(name + 2);
+
+
         }
 
         @Override
@@ -76,14 +91,14 @@ public class TmpActivity extends AppCompatActivity {
         }
     }
 
-    // 리사이클러 어뎁터에 연결할 ViewHolder
+    // xml 문서의 인스턴스와 연결
     private class MyViewHolder extends RecyclerView.ViewHolder {
-
-        public TextView name;
+        public TextView text1,text2;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            name = itemView.findViewById(R.id.tv_tmp);
+             text1 = itemView.findViewById(R.id.text1);
+             text2 = itemView.findViewById(R.id.text2);
         }
     }
 }
