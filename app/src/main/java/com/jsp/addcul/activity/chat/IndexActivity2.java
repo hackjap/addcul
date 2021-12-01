a955f702f10d6a5f4d2f7a1d9264679c9a54c52e
hackjap <hackjap1015@gmail.com>
2021-12-01 15:53:35 +0900

    채팅방 종류 ㅜ가

--- app/src/main/java/com/jsp/addcul/activity/chat/IndexActivity2.java
+++ app/src/main/java/com/jsp/addcul/activity/chat/IndexActivity2.java
@@ -51,7 +51,7 @@ public class IndexActivity2 extends AppCompatActivity {
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
     private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
     private FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-    private DatabaseReference conditionRef = databaseReference.child("message");
+    private DatabaseReference conditionRef = databaseReference.child("message2");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
