05a09aec2e5a76c430575b39ce71f94e9a1ea220
ChoiJunBae <cjb9610@naver.com>
2021-12-04 15:24:30 +0900

    채팅방 리스트 디자인 완료

---
+++ app/src/main/java/com/jsp/addcul/activity/ChatList.java
@@ -0,0 +1,16 @@
+package com.jsp.addcul.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jsp.addcul.R;
+
+public class ChatList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_list);
+    }
+}
